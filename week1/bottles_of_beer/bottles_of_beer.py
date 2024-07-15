2395687b128253a57309b91e3e2b342ceafb7dfd
20124796 <20124796@tafe.wa.edu.au>
2024-07-15 13:51:30 +0800

    finish session 1 in class

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,11 +1,21 @@
 bottles = 99
 
+
+def plurals(n):
+    """
+    Takes a number and returns the relevant plurality of the word bottle for that number.
+    :param n: The number of bottles.
+    :return: 'n bottles' if the number is not 1, otherwise 'n bottle'
+    """
+    return F"{n} bottle{"s" if n != 1 else ""}"
+
+
 while bottles > 0:
     print(f"""
-    {bottles} bottle{"s" if bottles > 1 else ""} of beer on the wall
-    {bottles} bottle{"s" if bottles > 1 else ""} of beer
+    {plurals(bottles)} of beer on the wall
+    {plurals(bottles)} of beer
     take {"one" if bottles > 1 else "it"} down, pass it around
-    {f"{bottles - 1} bottle{"s" if bottles > 1 else ""} of beer on the wall" if bottles > 1 else f"no more bottles of beer on the wall"}
+    {f"{plurals(bottles)} of beer on the wall" if bottles > 1 else f"no more bottles of beer on the wall"}
     
     """)
     bottles -= 1
