0aadc8556e2057bfa8a2f5a8f975512cc1278ca7
20124796 <20124796@tafe.wa.edu.au>
2024-07-15 11:50:04 +0800

    complete bottles of beer exercise

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,1 +1,11 @@
+bottles = 99
 
+while bottles > 0:
+    print(f"""
+    {bottles} bottle{"s" if bottles > 1 else ""} of beer on the wall
+    {bottles} bottle{"s" if bottles > 1 else ""} of beer
+    take {"one" if bottles > 1 else "it"} down, pass it around
+    {f"{bottles - 1} bottle{"s" if bottles > 1 else ""} of beer on the wall" if bottles > 1 else f"no more bottles of beer on the wall"}
+    
+    """)
+    bottles -= 1
