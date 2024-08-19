e4875d2b317658a6d8a6b3f5e1083d331247950c
20124796 <20124796@tafe.wa.edu.au>
2024-08-19 11:27:49 +0800

    feat: complete activity 2

--- week6/in_class_demos/data_structures/activities/activity_2_start.py
+++ week6/in_class_demos/data_structures/activities/activity_2_start.py
@@ -13,3 +13,31 @@
 
 # If the queue is empty, it returns None.
 # is_empty(): Returns True if the queue is empty, False otherwise.
+
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        elif not self.is_empty():
+            return self.queue.popleft()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        elif not self.is_empty():
+            return self.queue[0]
+
+    def is_empty(self):
+        if not self.queue:
+            return True
+        else:
+            return False
